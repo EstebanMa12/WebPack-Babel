621258f73af40b9d1e5ad5285aebefbfbcf76fb5
EstebanMa12 <desteban.maya@udea.edu.co>
2023-08-22 20:32:40 -0500

    Update webpack config to support hot module replacement.

--- webpack.config.js
+++ webpack.config.js
@@ -4,53 +4,53 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: {
-    main: path.resolve(__dirname, './src/index.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'main.bundle.js',
-  },
-  module: {
-    rules: [
-      // JavaScript
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      // Images
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
-      // Fonts and SVGs
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.bundle.js',
+    },
+    module: {
+        rules: [
+        // JavaScript
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ['babel-loader'],
+        },
+        // Images
+        {
+            test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+            type: 'asset/resource',
+        },
+        // Fonts and SVGs
+        {
+            test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+            type: 'asset/inline',
+        },
+        {
+            test: /\.(scss|css)$/,
+            use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        },
+        ],
+    },
+    mode: 'development',
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.join(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        title: 'webpack Boilerplate',
+        template: path.resolve(__dirname, './src/template.html'), // template file
+        filename: 'index.html', // output file
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
     ],
-  },
-  mode: 'development',
-  devServer: {
-    historyApiFallback: true,
-    contentBase: path.join(__dirname, './dist'),
-    open: true,
-    compress: true,
-    hot: true,
-    port: 8080,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'webpack Boilerplate',
-      template: path.resolve(__dirname, './src/template.html'), // template file
-      filename: 'index.html', // output file
-    }),
-    new CleanWebpackPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
 }
